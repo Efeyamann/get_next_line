6dad365ae52fe4fd183dd1c7301f5a3ffc1ad579
Efe Yaman Sir <esir@k1m18s03.42istanbul.com.tr>
2025-01-26 16:59:56 +0300

    ok actually the final commit

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: esir <esir@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 16:41:32 by esir              #+#    #+#             */
-/*   Updated: 2025/01/26 16:42:20 by esir             ###   ########.fr       */
+/*   Updated: 2025/01/26 16:57:21 by esir             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ size_t	ft_strlen(char *a);
 char	*ft_strncpy(char *dest, const char *src, size_t n);
 char	*get_next_line(int fd);
 char	*ft_strdup(char *src);
-int		check_newline(char *repo);
 char	*ft_strchr(const char *s, int c);
 
 #endif
